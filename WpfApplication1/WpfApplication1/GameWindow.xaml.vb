6bf76daadf8471d6eb07aec5567fe1dcd6a3df17
unknown <William.james.lawson@ADMINISTRATOR44.win.lithgow-h.schools.nsw.edu.au>
2015-07-16 13:04:42 +1000

    Commit(2)

---
+++ WpfApplication1/WpfApplication1/GameWindow.xaml.vb
@@ -0,0 +1,18 @@
+﻿Public Class GameWindow
+
+
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+
+        Dim fileReader As String
+
+        fileReader = My.Computer.FileSystem.ReadAllText("C:\Users\william.james.lawson\Desktop\test.txt")
+        TextScreen.Text = fileReader
+
+    End Sub
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+
+        Close()
+
+    End Sub
+End Class
---
+++ WpfApplication1/WpfApplication1/StartMenu.xaml.vb
@@ -0,0 +1,11 @@
+﻿Class StartMenu
+
+    Private Sub Start_Click(sender As Object, e As RoutedEventArgs) Handles Start.Click ''Goes to game window and closes the menu
+        Dim Frm As New GameWindow
+
+        Frm.Show()
+
+        Me.Close()
+
+    End Sub
+End Class
